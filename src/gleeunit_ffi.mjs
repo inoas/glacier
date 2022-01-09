2f2e8b18972ed7b55b3510f8178a5e8ced0ec171
Louis Pilfold <louis@lpil.uk>
2022-01-09 19:56:05 +0000

    JS runner

---
+++ src/gleeunit_ffi.mjs
@@ -0,0 +1,56 @@
+import { readdir, readFile } from "fs/promises";
+import { join as joinPath } from "path";
+
+async function* gleamFiles(directory) {
+  let dirents = await readdir(directory, { withFileTypes: true });
+  for (let dirent of dirents) {
+    let path = joinPath(directory, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* gleamFiles(path);
+    } else if (path.endsWith(".gleam")) {
+      yield path;
+    }
+  }
+}
+
+async function readRootPackageName() {
+  let toml = await readFile("gleam.toml", "utf-8");
+  for (let line of toml.split("\n")) {
+    let matches = line.match(/\s*name\s=\s"([a-z_]+)"/);
+    if (matches) return matches[1];
+  }
+  throw new Error("Could not determine package name from gleam.toml");
+}
+
+export async function main() {
+  let passes = 0;
+  let failures = 0;
+
+  let packageName = await readRootPackageName();
+  let dist = `../../${packageName}/dist/`;
+
+  for await (let path of await gleamFiles("test")) {
+    let js_path = path.slice("test/".length).replace(".gleam", ".mjs");
+    let module = await import(joinPath(dist, js_path));
+    for (let fnName of Object.keys(module)) {
+      if (!fnName.endsWith("_test")) continue;
+      try {
+        module[fnName]();
+        process.stdout.write(`\u001b[32m.\u001b[0m`);
+        passes++;
+      } catch (error) {
+        let moduleName = "\ngleam/" + entry.name.slice(0, -3);
+        process.stdout.write(`\n❌ ${moduleName}.${fnName}: ${error}\n`);
+        failures++;
+      }
+    }
+  }
+
+  console.log(`
+${passes + failures} tests, ${failures} failures`);
+  process.exit(failures ? 1 : 0);
+}
+
+export async function crash(message) {
+  throw new Error(message);
+}
