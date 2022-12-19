9997d98591528f06874cef821773e5bea841c7cc
inoas <mail@inoas.com>
2022-12-19 01:53:11 +0100

    cleanup

--- src/glacier_ffi.erl
+++ src/glacier_ffi.erl
@@ -1,26 +1,6 @@
 -module(glacier_ffi).
 
--export([stdout_println/1, list_map/2, string_replace/3, identity/1, start_watcher/1,
-         int_to_string/1]).
-
-stdout_println(Binary) ->
-    io:put_chars(Binary),
-    io:put_chars("\n").
-
-list_map(List, Callable) ->
-    lists:map(Callable, List).
-
-string_replace(String, SearchPattern, Replacement) ->
-    iolist_to_binary(string:replace(iolist_to_binary(String),
-                                    iolist_to_binary(SearchPattern),
-                                    iolist_to_binary(Replacement),
-                                    all)).
-
-identity(X) ->
-    X.
-
-int_to_string(Int) ->
-    integer_to_list(Int).
+-export([start_watcher/1]).
 
 start_watcher(Callback) ->
     {ok, Cwd} = file:get_cwd(),
