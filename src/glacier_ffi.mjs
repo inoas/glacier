2080986a01da8d9b50e17e938342077452a0e7ca
inoas <mail@inoas.com>
2022-12-29 00:06:33 +0100

    cleanup

--- src/glacier_ffi.mjs
+++ src/glacier_ffi.mjs
@@ -1,15 +1,3 @@
-export function stdout_println(binary) {
+export function foo(bar) {
 	return null;
-}
-
-export function list_map(list, callable) {
-	return null;
-}
-
-export function string_replace(string, search_pattern, replacement) {
-	return null;
-}
-
-export function identity(x) {
-  return x;
 }
