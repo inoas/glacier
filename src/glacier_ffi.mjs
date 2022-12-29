92a7015959814f0fdc67e08a3d628d553c52ff86
inoas <mail@inoas.com>
2022-12-29 01:31:34 +0100

    wip js impl

--- src/glacier_ffi.mjs
+++ src/glacier_ffi.mjs
@@ -1,7 +1,7 @@
 import * as Gleam from "./gleam.mjs";
 import * as Process from "node:process";
-// import { watch } from "node:fs/promises";
-const { watch } = require('node:fs/promises');
+const { watch } = require('node:fs/promises'); // import { watch } from "node:fs/promises";
+const fs = require('fs');
 
 export const argv = () => Gleam.List.fromArray(Process.argv.slice(1));
 
@@ -17,4 +17,28 @@ export const start_file_change_watcher = function (file_change_handler_fn) {
 			}
 		}
 	})();
+};
+
+export const read_file = function (absolute_file_name) {
+	try {
+		const data = fs.readFileSync(absolute_file_name, 'utf8');
+		// console.log(data);
+		return data;
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export const file_exists = function (absolute_file_name) {
+	if (fs.existsSync(absolute_file_name)) {
+		return true;
+	}
+	return false;
+};
+
+export const find_project_files = function () {
+	// find_project_files(Pattern, In) ->
+	//   Results = filelib:wildcard(binary_to_list(Pattern), binary_to_list(In)),
+	//   lists:map(fun list_to_binary/1, Results).
+	return Gleam.List.fromArray([]);
 };
