ec335ccd78669dfd07697983cd105a35ad31837d
inoas <mail@inoas.com>
2022-12-30 23:32:28 +0100

    cleanup

--- src/glacier_ffi.mjs
+++ src/glacier_ffi.mjs
@@ -1,14 +1,17 @@
+import * as Fs from "node:fs";
+import * as FsPromises from "node:fs/promises";
 import * as Gleam from "./gleam.mjs";
-import * as Process from "node:process";
-const { watch } = require('node:fs/promises'); // import { watch } from "node:fs/promises";
-const fs = require('fs');
+import * as NodeProcess from "node:process";
+import * as Path from "node:path";
 
 process.on('SIGINT', function () {
   console.log("\n🏔 Gracefully shutting down Glacier from SIGINT (Ctrl-C)!");
   process.exit(0);
 });
 
-process.on('warning', e => console.warn(e.stack));
+process.on('warning', function (e) {
+  console.warn(e.stack);
+});
 
 // require('events')
 // const emitter = new events.EventEmitter()
@@ -16,66 +19,63 @@ process.on('warning', e => console.warn(e.stack));
 // or 0 to turn off the limit
 // emitter.setMaxListeners(0)
 
-export const argv = () => Gleam.List.fromArray(Process.argv.slice(1));
+export const start_args = function () {
+  return Gleam.List.fromArray(NodeProcess.argv.slice(1));
+};
 
-export const cwd = () => Process.cwd();
+export const cwd = function () {
+  return NodeProcess.cwd();
+};
 
 export const start_file_change_watcher = function (file_change_handler_fn) {
-  watch_dir(cwd(), "/src", ["change"], file_change_handler_fn);
-  watch_dir(cwd(), "/test", ["change"], file_change_handler_fn);
+  watch_directory(cwd() + "/src", ["change"], file_change_handler_fn);
+  watch_directory(cwd() + "/test", ["change"], file_change_handler_fn);
 };
 
-async function watch_dir(base_dir, sub_dir, events, file_change_handler_fn) {
-  (async () => {
-    const watcher = watch(base_dir + sub_dir, { persistent: true, recursive: true });
-    for await (const event of watcher) {
-      if (events.includes(event.eventType)) {
-        let touched_file = base_dir + sub_dir + "/" + event.filename
-        console.log(touched_file);
-        file_change_handler_fn(touched_file);
-      }
+async function watch_directory(directory, events, file_change_handler_fn) {
+  const watcher = FsPromises.watch(directory, { persistent: true, recursive: true });
+  for await (const event of watcher) {
+    if (events.includes(event.eventType)) {
+      let touched_file = directory + "/" + event.filename
+      console.log(touched_file);
+      file_change_handler_fn(touched_file);
     }
-  })();
-};
-
-export const read_file = function (absolute_file_name) {
-  try {
-    const data = fs.readFileSync(absolute_file_name, 'utf8');
-    // console.log(data);
-    return data;
-  } catch (err) {
-    console.error(err);
   }
 };
 
+export const read_file = function (absolute_file_name) {
+  // try {
+  const data = Fs.readFileSync(absolute_file_name, 'utf8');
+  return data;
+  // } catch (err) {
+  //   console.error(err);
+  // }
+};
+
 export const file_exists = function (absolute_file_name) {
-  if (fs.existsSync(absolute_file_name)) {
+  if (Fs.existsSync(absolute_file_name)) {
     return true;
   }
   return false;
 };
 
-const path = require("path");
-export const find_files_recursive = function (file_exts_list, directory) {
+export const find_files_recursive_by_exts = function (directory, file_exts_list) {
   file_exts_list = file_exts_list.toArray();
-  // console.log(file_exts_list);
   let files = [];
-  const getFilesRecursively = (directory) => {
-    const filesInDirectory = fs.readdirSync(directory);
-    for (const file of filesInDirectory) {
-      const absolute = path.join(directory, file);
-      if (fs.statSync(absolute).isDirectory()) {
-        getFilesRecursively(absolute);
+  /* mut files */ const detect_files_recursive = function (directory) {
+    const files_in_directory = Fs.readdirSync(directory);
+    for (const file of files_in_directory) {
+      const absolute_path = Path.join(directory, file);
+      if (Fs.statSync(absolute_path).isDirectory()) {
+        detect_files_recursive(absolute_path);
       } else {
-        files.push(absolute);
+        files.push(absolute_path);
       }
     }
   };
-  getFilesRecursively(directory);
-
+  detect_files_recursive(directory);
   files = files.filter(function (absolute_file_name) {
     return absolute_file_name.endsWith(file_exts_list);
   });
-
   return Gleam.List.fromArray(files);
 };
